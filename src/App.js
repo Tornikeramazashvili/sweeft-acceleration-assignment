3737ed43cf8fb0efe046b1938c54205409ceb828
albus <mnateus@gmail.com>
2023-03-17 02:19:15 +0400

    fixed infinite scroll styles and cleaned up code

--- src/App.js
+++ src/App.js
@@ -5,45 +5,41 @@ import './App.css';
 
 function App() {
 
-  let offset = 0
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const URL = 'http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/10&offset='
 
-  const getAllUsers = () => {
-    axios.get(`http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/10&offset=${offset}`).then(({ data }) => {
-      const newUser = [];
-      data.list.forEach((user) => {
-        const { imageUrl, prefix, name, lastName, title } = user;
-        newUser.push({ imageUrl, prefix, name, lastName, title });
-      });
-      setUsers(olduser => [...olduser, ...newUser])
-    });
-    offset += 10;
+  const getAllUsers = async () => {
+    const { data } = await axios.get(`${URL}${offset}`);
+    const newUser = data.list.map(({ imageUrl, prefix, name, lastName, title }) => ({ imageUrl, prefix, name, lastName, title }));
+    setUsers(oldUsers => [...oldUsers, ...newUser]);
+    setOffset(offset + 10);
   };
 
   const handleScroll = (e) => {
-    if (
-      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
-      e.target.documentElement.scrollHeight
-    ) {
-      getAllUsers()
+    if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
+      getAllUsers();
     }
-  }
+  };
 
   useEffect(() => {
-    getAllUsers()
-    window.addEventListener('scroll', handleScroll)
-  }, [])
+    getAllUsers();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
-      <div className='users'>
-        {users.map((user, index) => (
-          <div key={index} className='user-container'>
-            <img className='user-image' src={user.imageUrl} alt='user' />
-            <p className='user-name'> {user.prefix} {user.name} {user.lastName}</p>
-            <p className='user-title'>{user.title}</p>
-          </div>
-        ))}
+      <div className='user-container'>
+        <div className='user-list'>
+          {users.map((user, index) => (
+            <div key={index} className='user'>
+              <img className='user-image' src={user.imageUrl} alt='user' />
+              <p className='user-name'> {user.prefix} {user.name} {user.lastName}</p>
+              <p className='user-title'>{user.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
