46692e28afcf57c881778eb93bf69031d315d953
albus <mnateus@gmail.com>
2023-03-16 16:59:02 +0400

    created infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  let offset = 0
+  const [users, setUsers] = useState([])
+
+  const getAllUsers = () => {
+    axios.get(`http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/10&offset=${offset}`).then(({ data }) => {
+      const newUser = [];
+      data.list.forEach((u) => newUser.push(u.name));
+      setUsers(olduser => [...olduser, ...newUser])
+    });
+    offset += 10;
+  };
+
+  const handleScroll = (e) => {
+    if (
+      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+      e.target.documentElement.scrollHeight
+    ) {
+      getAllUsers()
+    }
+  }
+
+  useEffect(() => {
+    getAllUsers()
+    window.addEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <ol>
+          {users.map((u, i) => (
+            <li className='li' key={i}>{u}</li>
+          ))}
+        </ol>
+      </div>
+    </>
   );
 }
 
