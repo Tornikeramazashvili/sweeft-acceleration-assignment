a1f55451ec645cd8e7050fcdab4551ea391c5356
albus <mnateus@gmail.com>
2023-03-18 00:54:45 +0400

    fixed fetching uses funcionality

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -8,20 +8,21 @@ import './Users.css';
 export const Users = () => {
 
     const [users, setUsers] = useState([]);
-    const [offset, setOffset] = useState(0);
-    const URL = 'http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/10&offset='
+    const [offset, setOffset] = useState(1);
+    const [size, setSize] = useState(10);
+    const URL = 'http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user'
     const navigate = useNavigate();
 
     const getAllUsers = async () => {
-        const { data } = await axios.get(`${URL}${offset}`);
+        const { data } = await axios.get(`${URL}/${offset}/${size}`);
         const newUser = data.list.map(({ id, imageUrl, prefix, name, lastName, title }) => ({ id, imageUrl, prefix, name, lastName, title }));
         setUsers(oldUsers => [...oldUsers, ...newUser]);
-        setOffset(offset + 10);
+
     };
 
     const handleScroll = (e) => {
         if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
-            getAllUsers();
+            setOffset(offset + 1)
         }
     };
 
@@ -29,7 +30,7 @@ export const Users = () => {
         getAllUsers();
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+    }, [offset]);
 
     return (
         <>
